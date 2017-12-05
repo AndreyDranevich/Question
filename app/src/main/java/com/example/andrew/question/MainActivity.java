6fd62bb82a549be09a9da070e3db5297e18a428a
Andrew <andran7@mail.ru>
2017-12-05 18:14:20 +0300

    update

--- app/src/main/java/com/example/andrew/question/MainActivity.java
+++ app/src/main/java/com/example/andrew/question/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.andrew.question;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,7 +14,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
